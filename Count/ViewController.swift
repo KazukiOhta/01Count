bae5c80b996914c506adbc98dc7ee8cda6006b12
KazukiOhta <kazukiota121@gmail.com>
2019-04-12 18:01:42 +0900

    完成

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -15,6 +15,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    var number:Int = 1
+    
+    @IBOutlet var number_label:UILabel!
 
+    @IBAction func reset(){
+        number = 1
+        common()
+    }
+    
+    @IBAction func double(){
+        number *= 2
+        common()
+    }
+    
+    @IBAction func divide(){
+        if (number % 3 == 1){
+            number -= 1
+            number /= 3
+        }
+        common()
+    }
+    
+    func common(){
+        number_label.text = String(number)
+    }
+    
 }
 
